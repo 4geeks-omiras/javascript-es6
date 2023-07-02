9840bfae34af0495c42d80e997cbf63f92e6d2db
Oscar Miras <oscar.miras@gmail.com>
2023-07-02 07:25:46 +0000

    updated arrow functions

--- .learn/resets/01-Arrow/app.js
+++
@@ -1,6 +0,0 @@
-function updateCounter(value) {
-    return value + 1
-}
-
-console.log(updateCounter(1)); // 2
-
--- exercises/01-Arrow-Functions/tests.js
+++ exercises/01-Arrow-Functions/tests.js
@@ -3,16 +3,14 @@ const fs = require('fs');
 const path = require('path');
 
 const file = rewire("./app.js");
-const getColor = file.__get__('updateCounter');
+const updateCounter = file.__get__('updateCounter');
 
 test('function updateCounter should exist', function () {
     expect(updateCounter).toBeTruthy();
 });
 
 test('The function "updateCounter" should increase the value of the parameter and return it', () => {
-    const file = rewire("./app.js");
-    const updateCounter = file.__get__("updateCounter");
-    expect(updateCounter(1).toBe(2)) ;
+    expect(updateCounter(1)).toBe(2) ;
 });
 
 test('You have to use => notation', () => {
